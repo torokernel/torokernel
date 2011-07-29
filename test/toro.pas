737b26b4845c2db0a7ae7b2ee8a3ca9ba888b6ae
Matias E. Vara <matiasvara@yahoo.com>
2011-07-29 13:13:40 -0300

    Second commit

---
+++ test/toro.pas
@@ -0,0 +1,128 @@
+//
+// Toro main program
+// Example how to use Toro in User Application.
+//
+// Changes :
+// 2008 / 01 / 01 : Toro 0.03
+// 2007 / 05 / 04 : Toro 0.02
+// 2007 / 01 / 16 : Toro 0.01
+//
+// Copyright (c) 2003-2011 Matias Vara <matiasvara@yahoo.com>
+// All Rights Reserved
+//
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+program toro;
+
+
+{$IFDEF FPC}
+ {$mode delphi}
+{$ENDIF}
+
+
+// Adding support for FPC 2.0.4 ;)
+{$IMAGEBASE 4194304}
+
+uses
+  Kernel in 'rtl\Kernel.pas',
+  Process in 'rtl\Process.pas',
+  Memory in 'rtl\Memory.pas',
+  Debug in 'rtl\Debug.pas',
+  Arch in 'rtl\Arch.pas',
+  Filesystem in 'rtl\Filesystem.pas',
+  NetWork in 'rtl\Network.pas',
+  Console in 'rtl\Drivers\Console.pas',
+  Ext2 in 'rtl\Drivers\Ext2.pas',
+  ne2000 in 'rtl\Drivers\ne2000.pas',
+  e1000 in 'rtl\Drivers\e1000.pas',
+  {$IFNDEF Linux}
+   //Libc in 'rtl\Libc.pas',
+   //SQLite3 in 'rtl\Library\SQLite3.pas',
+  {$ENDIF Linux}
+  IdeDisk in 'rtl\Drivers\IdeDisk.pas' // problems with this unit;
+  ;
+
+const
+  Welcome: PChar = 'Aca Toro64'+#13#10;
+  // TCP/IP Configuration
+  MaskIP: array[0..3] of Byte   = (255, 255, 255, 0);
+  Gateway: array[0..3] of Byte  = (192, 100, 200, 1);
+  LocalIP: array[0..3] of Byte  = (192, 100, 200, 100);
+
+
+var
+  HttpServer: PSocket;
+
+procedure HttpInit;
+begin
+  HttpServer := SysSocket(SOCKET_STREAM);
+  HttpServer.Sourceport := 80;
+  SysSocketListen(HttpServer, 50);
+end;
+
+function HttpAccept(Socket: PSocket): LongInt;
+begin
+  WriteConsole('New Connection on port 80\n',[0]);
+  // Waiting for new data from remote host or TimeOut or RemoteClose
+  SysSocketSend(Socket, Welcome, SizeOf(Welcome), 0);
+  SysSocketSelect(Socket, 500000);
+  Result := 0;
+end;
+
+ // new data received from Socket, we can read the data and return to Network Service thread
+function HttpReceive(Socket: PSocket): LongInt;
+begin
+  // the socket will dead
+  SysSocketClose(Socket);
+  Result := 0;
+end;
+
+ // Peer socket disconnected
+function HttpClose(Socket: PSocket): LongInt;
+begin
+  SysSocketClose(Socket);
+  Result := 0;
+end;
+
+ // TimeOut
+function HttpTimeOut(Socket: PSocket): LongInt;
+begin
+  SysSocketClose(Socket);
+  Result := 0;
+end;
+
+var
+  HttpHandler: TNetworkHandler;
+
+{$R *.res}
+
+begin
+  DedicateBlockDriver('ATA0', 0); // dedicate ATA0 driver to CPU#0
+  SysMount('ext2', 'ATA0', 6); // Mount ATA Master device , first partition with EXT2 FS
+  DedicateNetwork('e1000', LocalIP, Gateway, MaskIP, nil); // dedicate ne2000 network card to current CPU
+  WriteConsole('Running HTTP Service\n',[0]);
+
+  // Configuration of Handlers
+  HttpHandler.DoInit := @HttpInit;
+  HttpHandler.DoAccept := @HttpAccept;
+  HttpHandler.DoTimeOut := @HttpTimeOut;
+  HttpHandler.DoReceive := @HttpReceive;
+  HttpHandler.DoClose := @HttpClose;
+  SysRegisterNetworkService(@HttpHandler);
+
+  while True do
+    ThreadSwitch;
+end.

d3ef6a38f803d118ebf38048a1d2a9a0a2b0c275
Matias E. Vara <matiasvara@yahoo.com>
2011-08-24 20:28:06 -0300

    changing qemu parameters

--- test/qemu_toro_net.sh
+++ test/qemu_toro_net.sh
@@ -1,2 +1,4 @@
-sudo qemu-system-x86_64 -m 256 -hda toro.img -smp 2 -net nic,model=e1000 -serial file:serial.txt &
-#sudo tcpdump -i tap0 -X #host 192.100.200.100 -X
+#!/bin/sh
+iface=`sudo tunctl -b`
+sudo qemu-system-x86_64 -S -s -m 256 -hda toro.img -smp 2 -net nic,model=e1000 -serial file:serial.txt -net tap,ifname=$iface 
+#sudo tcpdump -i $iface -X #host 192.100.200.100 -X

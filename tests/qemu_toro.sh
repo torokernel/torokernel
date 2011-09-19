b67ea284046e04ebe624dbc974d325f795f279a4
Matias E. Vara <matiasvara@yahoo.com>
2011-09-18 22:42:34 -0300

    Renaming tests files

--- tests/qemu_toro.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-x86_64 -m 256 -hda toro.img -smp 2 -net nic,model=e1000 -serial file:serial.txt
--- tests/qemu_toro_net.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-make clean
-make DEBUG_INFO=1
-iface=`sudo tunctl -b`
-sudo qemu-system-x86_64 -s -S -m 256 -hda toro.img -smp 2 -net nic,model=e1000 -serial file:serial.txt -net tap,ifname=$iface &
-../../gdb-7.3/gdb/gdb Toro 
-#sudo tcpdump -i $iface -X host 192.100.200.100 

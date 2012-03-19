c90bda091199c2da8331f2337552a510429bed41
unknown <Administrator@los-e4aqvfal41f.(none)>
2012-03-18 21:39:37 -0300

    adding support for mingw

--- tests/ToroNetwork2.sh
+++ tests/ToroNetwork2.sh
@@ -1,34 +1,45 @@
 #!/bin/sh
-app="ToroNetwork2"
-appsource="$app.pas"
-appexe=$app
-appimg="$app.img"
+app="ToroNetwork2";
+appsource="$app.pas";
+appbin=$app;
+appimg="$app.img";
 debug=false;
 emulate=false;
+qemubios="c:/qemu-1.0/pc-bios/";
+gdbdir="C:/mingw64/1.0/home/gdb-7.1/gdb/";
 # checking the command line
 while [ $# -gt 0 ]
 do
     case "$1" in
-        -d)  debug=true;;
- 	-e)  emulate=true;;
+		-d)  debug=true;;
+		-e)  emulate=true;;
     esac
     shift
 done
-# making the magic
+# debug or just emulate?
 if $debug ; then
-	fpc $appsource -g -Fu../rtl/ -Fu../rtl/drivers
-	./build 2 $appexe boot.o $appimg
- 	sudo qemu-system-x86_64 -s -S -m 256 -hda $appimg -smp 2 -net nic,model=ne2k_pci -net tap,ifname=$iface &
-        # at this point we need a gdb patched
-	../../gdb-7.3/gdb/gdb $appexe 
+	fpc $appsource -o$appbin -g -Fu../rtl/ -Fu../rtl/drivers
+	./build 2 $appbin boot.o $appimg
+	# calling qemu
+	if [ "$OSTYPE" == "msys" ] ; then
+		qemu-system-x86_64 -s -S -L $qemubios -m 256 -hda $appimg -smp 2 -net nic,model=ne2k_pci -net tap,ifname=TAP2 &
+	elif [ "$OSTYPE" == "linux" ] ; then
+		iface=`sudo tunctl -b`
+		qemu-system-x86_64 -s -S -m 256 -hda $appimg -smp 2 -net nic,model=ne2k_pci -net tap,ifname=$iface &
+	fi
+	# at this point we need a gdb patched
+	$gdbdir/gdb $appbin
 else
-       # calling the compiler
-       fpc $appsource -Fu../rtl/ -Fu../rtl/drivers
-	./build 2 $appexe boot.o $appimg
-       # calling qemu as emulator
+	# compiling
+	fpc $appsource -o$appbin -Fu../rtl/ -Fu../rtl/drivers
+	./build 2 $appbin boot.o $appimg
+	# calling qemu
 	if $emulate ; then
-                iface=`sudo tunctl -b`
-	 	sudo qemu-system-x86_64 -m 256 -hda $appimg -smp 2 -net nic,model=ne2k_pci -net tap,ifname=$iface
-        fi
-fi 
-
+		if [ "$OSTYPE" == "msys" ] ; then
+			qemu-system-x86_64 -L $qemubios -m 256 -hda $appimg -smp 2 -net nic,model=ne2k_pci -net tap,ifname=TAP2
+	    elif [ "$OSTYPE" == "linux" ] ; then
+			iface=`sudo tunctl -b`
+			qemu-system-x86_64 -m 256 -hda $appimg -smp 2 -net nic,model=ne2k_pci -net tap,ifname=$iface
+		fi
+    fi
+fi

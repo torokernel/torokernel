9bed168bfb01cbc24928f23ebe643f979536ad47
Matias Vara <matiasevara@gmail.com>
2016-10-08 13:36:44 +0100

    Adding CloudIt.sh
    So far we were using one script per
    application. This script replaces such a way to compile and
    run an application in a VM instance.

---
+++ tests/CloudIt.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# CloudIt.sh <name>
+#
+# This script compiles the freepascal application in <name>
+# within the Toro Kernel. This results in a disk image named 
+# <name>.img. Then, this image is used to run a instance of 
+# the app in a VM in QEMU. 
+# 
+# TODO: Parameters of QEMU should vary depending on the app.
+#		It should be interested to have a separatly file that 
+#		tells information about the platform.  
+#
+app="$1";
+appsource="$app.pas";
+appbin=$app;
+appimg="$app.img";
+
+# compiling
+fpc $appsource -o$appbin -Fu../rtl/ -Fu../rtl/drivers
+./build 2 $appbin boot.o $appimg
+# calling qemu
+if [ "$OSTYPE" == "msys" ] ; then
+	qemu-system-x86_64 -m 256 -hda $appimg -smp 2
+	  elif [ "$OSTYPE" == "linux-gnu" ] ; then
+	qemu-system-x86_64 -m 256 -hda $appimg -smp 2
+fi

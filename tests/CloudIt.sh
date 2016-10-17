b5097f4a68a7815bd6ba63bff5240dbf50fa75a4
Matias Vara <matiasevara@gmail.com>
2016-10-17 22:30:36 +0100

    QEMU parameters are pased by using a separed file
    I modified CloudIt.sh to get the qemu parameters by
    using the corresponding file with .qemu extension.

--- tests/CloudIt.sh
+++ tests/CloudIt.sh
@@ -7,21 +7,19 @@
 # <name>.img. Then, this image is used to run a instance of 
 # the app in a VM in QEMU. 
 # 
-# TODO: Parameters of QEMU should vary depending on the app.
-#		It should be interested to have a separatly file that 
-#		tells information about the platform.  
-#
+
 app="$1";
 appsource="$app.pas";
 appbin=$app;
 appimg="$app.img";
+qemufile="$app.qemu"
 
-# compiling
+# we get the parameters
+qemuparam=`cat $qemufile`
+
+# we compile the application
 fpc $appsource -o$appbin -Fu../rtl/ -Fu../rtl/drivers
 ./build 2 $appbin boot.o $appimg
-# calling qemu
-#if [ "$OSTYPE" == "msys" ] ; then
-	qemu-system-x86_64 -m 256 -hda $appimg -smp 2
-#	  elif [ "$OSTYPE" == "linux-gnu" ] ; then
-#	qemu-system-x86_64 -m 256 -hda $appimg -smp 2
-#fi
+
+# we run the application in qemu
+qemu-system-x86_64 $qemuparam -hda $appimg

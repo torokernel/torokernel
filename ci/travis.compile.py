8f78766d0aaa6c8bb852e3eb8b91f0b10c62ac32
Matias Vara <matiasevara@gmail.com>
2018-08-21 17:16:29 +0200

    Issue #231: Compile all examples during ci

--- ci/travis.compile.py
+++ ci/travis.compile.py
@@ -5,10 +5,20 @@ import os
 import subprocess
 
 def main():
-    os.chdir (os.path.pardir + '/builder')
-    return os.system ('fpc build.pas') 
-    # contruir el builder
-    # construir los examples
+    cwd = os.getcwd()
+    # build the builder
+    os.chdir (cwd + '/builder')
+    os.system ('fpc build.pas') 
+    os.chdir (cwd)
+    # build all the examples
+    os.chdir (cwd + '/examples')
+    allfiles = os.listdir('.')
+    for names in allfiles:
+        if names.endswith('.pas'):
+              os.system ('fpc -TLinux -O2 ' + names + ' -o' + names[:-4] +' -Fu../rtl/ -Fu../rtl/drivers -MObjfpc') 
+              os.system ('../builder/build 4 ' + names[:-4] +' ../builder/boot.o ' + names[:-4] + '.img')
+    return os.chdir(cwd)
+    
 
 if __name__ == '__main__':
     sys.exit(int(not main()))

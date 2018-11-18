be98f95b92c4d997cb86fc03f4d800ca7c5db37e
Matias Vara <matiasevara@gmail.com>
2018-11-18 12:38:25 +0100

    Fix CI for new directory structure

--- ci/travis.compile.py
+++ ci/travis.compile.py
@@ -44,6 +44,7 @@ def main():
                     os.chdir (cwd + '/examples' + '/'+ dir)
 		    os.system ('fpc -TLinux -O2 ' + names + ' -o' + names[:-4] +' -Fu../../rtl/ -Fu../../rtl/drivers -MObjfpc') 
                     os.system ('../../builder/build 4 ' + names[:-4] +' ../../builder/boot.o ' + names[:-4] + '.img')
+                    os.system ('sha256sum ' + names[:-4] + '.img' + ' > ' + names[:-4] + '.img.sha256')
                     os.chdir (cwd + '/examples')
     os.chdir(cwd)
     return 1

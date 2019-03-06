b0cef5db6eff824c02d6cad43acee8e019ebecc3
Matias Vara <matiasevara@gmail.com>
2019-03-06 21:44:46 +0000

    Ensure full code coverage

--- tests/Memory/TestMemory.pas
+++ tests/Memory/TestMemory.pas
@@ -1,7 +1,7 @@
 //
 // TestMemory.pas
 //
-// This unit contains unit-tests of the Memory unit. 
+// This unit contains unittests for the Memory unit.
 // 
 // Copyright (c) 2003-2019 Matias Vara <matiasevara@gmail.com>
 // All Rights Reserved
@@ -43,17 +43,32 @@ uses
 var
   test: LongInt;
 
+// This function tests ToroGetMem()
+// The function ensures full code coverage
 function TestGetMem(out test: Longint): Boolean;
+var
+  i: LongInt;
 begin
   Result := False;
   test := 0;
+  i := 0;
+  // zero allocation returns the small chunk
   If ToroGetMem(0) = nil then
     Exit;
-  Inc(test);  
-  If ToroGetMem(1) = nil then
+  Inc(test);
+  // Size > MAX_BLOCKSIZE  it shall return nil
+  If ToroGetMem(2*1024*1024*1024) <> nil then
     Exit;
   Inc(test);
-  If ToroGetMem(2*1024*1024*1024) <> nil then
+  // get all 64 bytes blocks
+  while true do
+  begin
+    if ToroGetMem(64) = nil then
+      Break;
+    i += 1;
+  end;
+  // number of allocations of 64 bytes for 256Mb per core
+  if i <> 1765080 Then
     Exit;
   Result := True; 
 end;
@@ -74,41 +89,17 @@ begin
   Result := True;
 end;
 
-function TestStressMemory(out test: Longint): Boolean;
-var
-  i: Longint;
-begin
-  Result := False;
-  test := 0;
-  i := 0;
-  while true do
-  begin
-    if ToroGetMem(64) = nil then
-      Break;
-    i += 1;
-  end;
-  // number of allocations of 64 bytes for 256Mb per core
-  if i <> 1765080 Then
-    Exit;
-  Result := True;
-end;
-
 begin
   // TODO: Neither exceptions nor panics are captured
-  if TestGetMem(test) then
-    WriteDebug('TestGetMem: PASSED\n', [])
-  else
-    WriteDebug('TestGetMem: FAILED, %d\n', [test]);
-
   if TestFreeMem(test) then
     WriteDebug('TestFreeMem: PASSED\n', [])
   else
     WriteDebug('TestFreeMem: FAILED\n', []);
-  
-  if TestStressMemory(test) then
-    WriteDebug('TestStressMemory: PASSED\n', [])
+
+  if TestGetMem(test) then
+    WriteDebug('TestGetMem: PASSED\n', [])
   else
-    WriteDebug('TestStressMemory: FAILED\n', []);
+    WriteDebug('TestGetMem: FAILED, %d\n', [test]);
 
   ShutdownInQemu;
 end.

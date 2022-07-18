1cfe68fdd78d9c35d43e80a500bd71b097ba4051
Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>
2022-07-18 22:08:33 +0000

    Example: Add MpiReduce example
    Signed-off-by: Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>

---
+++ examples/MPI/MpiReduce.pas
@@ -0,0 +1,83 @@
+// MpiReduce.pas
+//
+// This example shows the use of MPI_REDUCE to reduce a vector
+// by using the MPI_SUM reduction.
+//
+// Copyright (c) 2003-2022 Matias Vara <matiasevara@gmail.com>
+// All Rights Reserved
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+program MpiReduce;
+
+uses
+ Kernel,
+ Process,
+ Memory,
+ Debug,
+ Arch,
+ Filesystem,
+ Network,
+ VirtIO,
+ {$IFDEF UseGDBstub}
+ VirtIOConsole,
+ Gdbstub,
+ {$ENDIF}
+ ToroMPI,
+ Console;
+
+const
+  VECTOR_LEN = 64;
+
+var
+  root: LongInt = 0;
+
+function Main(param: Pointer): PtrInt;
+var
+  r: ^LongInt;
+  s: ^LongInt;
+  len, i, rank: LongInt;
+begin
+  // build a vector with the rank of each core
+  // and reduce it with the MPI_SUM operation
+  rank := GetApicId;
+  r := ToroGetMem(VECTOR_LEN * sizeof(LongInt));
+  s := ToroGetMem(VECTOR_LEN * sizeof(LongInt));
+  for i:= 0 to VECTOR_LEN-1 do
+    r[i] := rank;
+  Mpi_Reduce(@r[0], @s[0], VECTOR_LEN, MPI_SUM, root);
+  if rank = root then
+  begin
+    for i:= 0 to VECTOR_LEN-1 do
+      if s[i] <> (((CPU_COUNT-1) * CPU_COUNT) div 2) then
+      begin
+        WriteConsoleF('Test has failed\n',[]);
+        Break;
+      end;
+    if i = VECTOR_LEN-1 then
+      WriteConsoleF('Test has succeeded\n',[]);
+  end;
+  FreeMem(r);
+  FreeMem(s);
+end;
+
+var
+  i: LongInt;
+  tmp: QWORD;
+begin
+ for i:= 0 to CPU_COUNT-1 do
+   tmp := BeginThread(nil, 4096, @Main, Nil, i, tmp);
+ while True do ThreadSwitch;
+end.
---
+++ examples/MPI/ToroMPI.pas
@@ -0,0 +1,118 @@
+Unit ToroMPI;
+
+interface
+
+uses
+  Arch,
+  Memory,
+  Console,
+  VirtIOBus;
+
+const
+  MPI_SUM = 0;
+  MPI_MIN = 1;
+
+function Mpi_Scatter(send_data: Pointer; send_count: LongInt; recv_data: Pointer; var recv_count: LongInt; root: LongInt): LongInt;
+function Mpi_Gather(send_data: Pointer; send_count: LongInt; recv_data: Pointer; var recv_count: LongInt; root: LongInt): LongInt;
+function Mpi_Reduce(send_data: Pointer; recv_data: Pointer; send_count: LongInt; Operation: Longint; root:LongInt): LongInt;
+
+implementation
+
+// assume that data-type are longint
+function Mpi_Scatter(send_data: Pointer; send_count: LongInt; recv_data: Pointer; var recv_count: LongInt; root: LongInt): LongInt;
+var
+  r, tmp: ^LongInt;
+  cpu, len_per_core: LongInt;
+  buff: array[0..VIRTIO_CPU_MAX_PKT_BUF_SIZE-1] of Char;
+begin
+  r := send_data;
+  len_per_core := (send_count * sizeof(LongInt)) div CPU_COUNT;
+  if GetApicID = root then
+  begin
+    for cpu:= 0 to CPU_COUNT-1 do
+    begin
+      if cpu <> root then
+        SendTo(cpu, r, len_per_core)
+      else
+      begin
+        tmp := recv_data;
+        recv_count := len_per_core;
+        Move(r^, tmp^, len_per_core);
+      end;
+      Inc(r, len_per_core div (sizeof(longint)));
+    end;
+  end else
+  begin
+    RecvFrom(root, @buff[0]);
+    tmp := recv_data;
+    r := @buff[0];
+    Move(r^, tmp^, len_per_core);
+    recv_count := len_per_core;
+  end;
+end;
+
+function Mpi_Gather(send_data: Pointer; send_count: LongInt; recv_data: Pointer; var recv_count: LongInt; root: LongInt): LongInt;
+var
+  r, tmp: ^LongInt;
+  cpu, len_per_core: LongInt;
+  buff: array[0..VIRTIO_CPU_MAX_PKT_BUF_SIZE-1] of Char;
+begin
+  r := recv_data;
+  len_per_core := send_count * sizeof(LongInt);
+  if GetApicID = root then
+  begin
+    for cpu:= 0 to CPU_COUNT-1 do
+    begin
+      if cpu <> root then
+      begin
+        RecvFrom(cpu, @buff[0]);
+        tmp := @buff[0];
+        Move(tmp^, r^, len_per_core);
+      end
+      else
+      begin
+        tmp := send_data;
+        Move(tmp^, r^, len_per_core);
+      end;
+      Inc(r, len_per_core div sizeof(LongInt));
+    end;
+  end else
+    SendTo(root, send_data, len_per_core);
+end;
+
+function Mpi_Reduce(send_data: Pointer; recv_data: Pointer; send_count: LongInt; Operation: Longint; root:LongInt): LongInt;
+var
+  count, i, j, len: LongInt;
+  ret, s: ^LongInt;
+begin
+  count := 0;
+  if GetApicID = root then
+    s := ToroGetMem(sizeof(LongInt) * CPU_COUNT * send_count)
+  else
+    s := nil;
+  Mpi_Gather(send_data, send_count, s, len, root);
+  if GetApicId = root then
+  begin
+    ret := recv_data;
+    if Operation = MPI_SUM then
+    begin
+      for j := 0 to send_count -1 do
+      begin
+        ret[j] := 0;
+        for i := 0 to CPU_COUNT-1 do
+          Inc(ret[j], s[i * send_count + j]);
+      end;
+    end else if Operation = MPI_MIN then
+    begin
+      for j := 0 to send_count -1 do
+      begin
+        ret[j] := MaxLongint;
+        for i := 0 to CPU_COUNT-1 do
+          if ret[j] > s[i * send_count + j] then
+            ret[j] := s[i * send_count +j];
+      end;
+    end;
+    ToroFreeMem(s);
+  end;
+end;
+end.

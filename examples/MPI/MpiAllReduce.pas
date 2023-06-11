904792745d30ce7554b25ea85458bae2544aeda4
Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>
2023-06-11 21:54:38 +0000

    Examples/MPI: Add MPI_AllReduce example (#524)
    Signed-off-by: Matias Ezequiel Vara Larsen <matiasevara@torokernel.io>

---
+++ examples/MPI/MpiAllReduce.pas
@@ -0,0 +1,49 @@
+// MpiAllReduce.pas
+//
+// Copyright (c) 2003-2023 Matias Vara <matiasevara@torokernel.io>
+// All Rights Reserved
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+program MpiReduce;
+
+uses
+ Kernel,
+ Process,
+ Memory,
+ Debug,
+ Arch,
+ Filesystem,
+ Network,
+ VirtIO,
+ {$IFDEF UseGDBstub}
+ VirtIOConsole,
+ Gdbstub,
+ {$ENDIF}
+ ToroMPI,
+ Console;
+
+{$L MPI_AllReduce.o}
+
+function mainC(param: Pointer): PtrInt; external name 'mainC';
+
+var
+  i: LongInt;
+  tmp: QWORD;
+begin
+ for i:= 0 to CPU_COUNT-1 do
+   tmp := BeginThread(nil, 4096, @mainC, Nil, i, tmp);
+ while True do ThreadSwitch;
+end.
